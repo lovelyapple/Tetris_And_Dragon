d4ec6484e75aa084bbd3d29d7a3737db90b6fe30
lovelyapple <lovelyappleoffice@gmail.com>
2016-03-10 11:48:05 +0900

    Create Battle_Effect_TimeLeftCount.h

---
+++ Battle_Effect_TimeLeftCount.h
@@ -0,0 +1,13 @@
+//====================================================
+//Battle_Effect_TimeLeftCount.h
+//====================================================
+#ifndef _BATTLE_EFFECT_TIME_LEFT_COUNT_
+#define _BATTLE_EFFECT_TIME_LEFT_COUNT_
+#include "main.h"
+HRESULT InitBattle_Effect_TimeLeftCount(void);
+void UninitBattle_Effect_TimeLeftCount(void);
+void UpdateBattle_Effect_TimeLeftCount(void);
+void DrawBattle_Effect_TimeLeftCount(void);
+
+
+#endif
